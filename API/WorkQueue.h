4cb30bddde53dff03fc39f2d651a22b07b6e9154
Said Gadjiev <gsarma.1995@mail.ru>
2016-03-10 22:21:09 +0300

    Workqueue

--- API/WorkQueue.h
+++ API/WorkQueue.h
@@ -5,10 +5,37 @@
 #ifndef HTTPSERVER_WORKQUEUE_H
 #define HTTPSERVER_WORKQUEUE_H
 
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-class WorkQueue {
-
-};
+namespace http {
+	namespace server {
+		typedef struct worker {
+			pthread_t thread;
+			int terminate;
+			struct workqueue *workqueue;
+			struct worker *prev;
+			struct worker *next;
+		} worker_t;
+		typedef struct job {
+			void (*job_function)(struct job *job);
+			void *user_data;
+			struct job *prev;
+			struct job *next;
+		} job_t;
+		typedef struct workqueue {
+			struct worker *workers;
+			struct job *waiting_jobs;
+			pthread_mutex_t jobs_mutex;
+			pthread_cond_t jobs_cond;
+		} workqueue_t;
+		int workqueue_init(workqueue_t *workqueue, int numWorkers);
+		void workqueue_shutdown(workqueue_t *workqueue);
+		void workqueue_add_job(workqueue_t *workqueue, job_t *job);
+	}
+}
 
 
 #endif //HTTPSERVER_WORKQUEUE_H
